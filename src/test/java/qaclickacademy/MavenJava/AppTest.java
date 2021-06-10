5acb213301b49cd92f09685f28ddca97d38697e9
Pranjal <pranjal.korde4999@gmail.com>
2021-06-10 06:16:01 +0530

    first message

---
+++ src/test/java/qaclickacademy/MavenJava/AppTest.java
@@ -0,0 +1,10 @@
+package qaclickacademy.MavenJava;
+
+public class AppTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
---
+++ src/test/java/qaclickacademy/MavenJava/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeApp() {
+		System.out.println("NativeApp");
+	}
+	@Test
+	public void IosApp() {
+		System.out.println("IosApp");
+		
+	}
+
+}
